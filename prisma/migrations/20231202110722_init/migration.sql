ceb2610143cb53554e2290ef5555c59c2093af44
Evgeny Paromov <evgeny.paromov@evrone.com>
2023-12-02 14:14:02 +0300

    chore: add prisma

---
+++ prisma/migrations/20231202110722_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const client = new PrismaClient();
+
+export default async function Home() {
+  const courses = await client.course.findMany();
+
+  console.log(courses);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
