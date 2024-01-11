a804ac5324d50e83ab8f5e10b88fb6be9ce9d933
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-01-11 09:02:34 +0300

    feat: init next-auth

---
+++ src/entities/session/next-auth-config.ts
@@ -0,0 +1,14 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { dbClient } from "@/shared/lib/db";
+
+export const nextAuthConfig: AuthOptions = {
+  adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+};
