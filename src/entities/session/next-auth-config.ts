3b734f33e79f6666af227ba6a338a567c587fcc9
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-01-13 11:19:43 +0300

    feat: add email auth

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { dbClient } from "@/shared/lib/db";
 import { compact } from "lodash-es";
@@ -8,6 +9,17 @@ import { privateConfig } from "@/shared/config/private";
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions["adapter"],
   providers: compact([
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST,
+        port: privateConfig.EMAIL_SERVER_PORT,
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: privateConfig.EMAIL_FROM,
+    }),
     privateConfig.GITHUB_ID &&
       privateConfig.GITHUB_SECRET &&
       GithubProvider({
