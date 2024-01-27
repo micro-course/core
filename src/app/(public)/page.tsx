7fa2f4bbbdc65c437818eaa9e12cbdd2a06a407e
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-01-27 12:11:00 +0300

    chore: Add local nginx content hosting

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import { CoursesList } from "@/features/courses-list/courses-list";
 
+export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col  p-8">
