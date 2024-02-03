76080237c369f8b849f91f0795ab7f31d6ef4995
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-02-03 14:17:04 +0300

    add save latest viewport

--- src/features/courses-map/_ui/flow.tsx
+++ src/features/courses-map/_ui/flow.tsx
@@ -3,17 +3,33 @@ import { useCallback, useEffect, useMemo } from "react";
 import ReactFlow, {
   useNodesState,
   useEdgesState,
-  addEdge,
-  Connection,
   Controls,
   MiniMap,
   Background,
   BackgroundVariant,
+  useReactFlow,
 } from "reactflow";
 import { MapProjection } from "../_domain/projections";
 import { customNodes } from "./nodes/custom-nodes";
 import { getFlowNode } from "../_vm/data-prepare/flow-nodes/get-flow-node";
 import { useMoveNode } from "../_vm/actions/use-move-node";
+import { SafeLocalStorage } from "@/shared/lib/safe-local-storage";
+import { z } from "zod";
+
+const flowKey = "react-flow";
+
+const viewportStorage = new SafeLocalStorage(
+  "viewport",
+  z.union([
+    z.object({
+      x: z.number(),
+      y: z.number(),
+      zoom: z.number(),
+    }),
+    z.undefined(),
+  ]),
+  undefined,
+);
 
 export function Flow({ map }: { map: MapProjection }) {
   const initialNodes = useMemo(
@@ -27,23 +43,29 @@ export function Flow({ map }: { map: MapProjection }) {
       ),
     [map],
   );
+  const flow = useReactFlow();
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, , onEdgesChange] = useEdgesState([]);
+
   const { move } = useMoveNode();
+
+  const onSave = useCallback(() => {
+    const flowObj = flow.toObject();
+    viewportStorage.set(flowObj.viewport);
+  }, [flow]);
 
   useEffect(() => {
     setNodes(initialNodes);
   }, [initialNodes, setNodes]);
 
-  const onConnect = useCallback(
-    (params: Connection) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
-  );
-
   return (
     <div className="absolute inset-0">
       <ReactFlow
+        fitView={!viewportStorage.get()}
+        defaultViewport={viewportStorage.get()}
+        onlyRenderVisibleElements={true}
+        onMoveEnd={onSave}
         nodes={nodes}
         edges={edges}
         nodeTypes={customNodes}
@@ -51,7 +73,7 @@ export function Flow({ map }: { map: MapProjection }) {
         onEdgesChange={onEdgesChange}
         maxZoom={10}
         minZoom={0.01}
-        onConnect={onConnect}
+        onNodesDelete={console.log.bind(null, "nodes delete")}
         onNodeDragStop={(_, node) => {
           console.log("move");
           move({ x: node.position.x, y: node.position.y, id: node.id });
