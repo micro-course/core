3d78c6822b0c3e0cfa702e33f889a9626ae47730
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-03-03 01:54:52 +0300

    feat(theme): Disable theme toggle

--- src/features/theme/theme-provider.tsx
+++ src/features/theme/theme-provider.tsx
@@ -7,7 +7,7 @@ export function ThemeProvider({ children }: { children?: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
     >
