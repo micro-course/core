89f72a355b45a18563a3ccb560e5c1175c72995d
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-03-04 23:51:36 +0300

    feat: Add help button

--- src/widgets/app-header/app-header.tsx
+++ src/widgets/app-header/app-header.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/shared/ui/button";
 import { Layout } from "./_ui/layout";
 import { Logo } from "./_ui/logo";
 import { MainNav } from "./_ui/main-nav";
 import { Profile } from "./_ui/profile";
+import { HelpCircle } from "lucide-react";
 
 export function AppHeader({
   variant,
@@ -14,6 +16,13 @@ export function AppHeader({
       logo={<Logo />}
       nav={<MainNav />}
       profile={isProfile && <Profile />}
+      actions={
+        <Button size={"icon"} variant={"ghost"} asChild>
+          <a href="https://t.me/microcourses_support">
+            <HelpCircle className="h-5 w-5" />
+          </a>
+        </Button>
+      }
     />
   );
 }
