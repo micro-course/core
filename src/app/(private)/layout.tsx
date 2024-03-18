4a49a07fe6255354ad6597602285603e59af61ad
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-03-18 07:32:47 +0300

    chore: remove widgets

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,5 +1,5 @@
 import AuthorizedGuard from "@/features/auth/authorized-guard";
-import { AppHeader } from "@/widgets/app-header/app-header";
+import { AppHeader } from "../_widgets/app-header/app-header";
 
 export default async function Layout({
   children,
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,4 +1,4 @@
-import { AppHeader } from "@/widgets/app-header/app-header";
+import { AppHeader } from "../_widgets/app-header/app-header";
 
 export default async function Layout({
   children,
