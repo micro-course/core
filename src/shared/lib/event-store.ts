cf54ac24ffe83158e1ba3d1adb909f45bb274063
Evgeny Paromov <evgeny.paromov@evrone.com>
2024-10-15 17:06:04 +0300

    fix: event store connection

--- src/shared/lib/event-store.ts
+++ src/shared/lib/event-store.ts
@@ -1,4 +1,6 @@
 import { EventStoreDBClient } from "@eventstore/db-client";
 import { privateConfig } from "../config/private";
 
-export const eventStoreDb = EventStoreDBClient.connectionString`${privateConfig.EVENT_STORE_DB_URL}`;
+export const eventStoreDb = new EventStoreDBClient({
+  endpoint: privateConfig.EVENT_STORE_DB_URL,
+});
